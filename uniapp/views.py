419385f58ba07bc0d84384eea7b6d242f3ac41b2
MANMOHAN771998 <manmohansingh771998@gmail.com>
2024-02-13 09:11:27 +0530

    added on base and created views

--- uniapp/views.py
+++ uniapp/views.py
@@ -3,3 +3,27 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
+
+def qa_services(request):
+    return render(request, 'index.html')
+
+def web_dev(request):
+    return render(request, 'index.html')
+
+def publishing(request):
+    return render(request, 'index.html')
+
+def seo(request):
+    return render(request, 'index.html')
+
+def recruitment(request):
+    return render(request, 'index.html')
+
+def timecodes_translation(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about-us.html')
+
+def contact(request):
+    return render(request, 'contact.html')
